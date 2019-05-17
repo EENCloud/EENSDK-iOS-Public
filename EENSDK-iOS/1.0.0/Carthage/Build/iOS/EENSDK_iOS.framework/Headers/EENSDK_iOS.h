e8ab671d2f9991ba708bd5a66ea7b33558b83310
Oana Corb <ocorb@eagleeyenetworks.com>
2019-05-17 18:15:42 +0200

    Added carthage files

---
+++ EENSDK-iOS/1.0.0/Carthage/Build/iOS/EENSDK_iOS.framework/Headers/EENSDK_iOS.h
@@ -0,0 +1,17 @@
+//
+//  EENSDK_iOS.h
+//  EENSDK-iOS
+//
+//  Created by D. Ianchyk on 19/11/2018.
+//  Copyright © 2018 Eagle Eye Networks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EENSDK_iOS.
+FOUNDATION_EXPORT double EENSDK_iOSVersionNumber;
+
+//! Project version string for EENSDK_iOS.
+FOUNDATION_EXPORT const unsigned char EENSDK_iOSVersionString[];
+
+#import <EENSDK_iOS/EENMediaPlayer.h>
